1b1313e03eed49fd1cd40c8069c4758af552c812
Pil <70975556+arnaudpilato@users.noreply.github.com>
2022-01-12 11:15:37 +0100

    initialize project

---
+++ src/main/java/com/wildcodeschool/original_diy/OriginalDiyApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.original_diy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginalDiyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginalDiyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/original_diy/OriginalDiyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.original_diy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginalDiyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
