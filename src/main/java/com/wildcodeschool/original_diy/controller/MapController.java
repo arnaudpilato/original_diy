6881a4b1d84accc580b736884e91b5897cb280d3
Tisserandmat <tisserand.mat@gmail.com>
2022-01-21 10:59:54 +0100

    Réglages du UriBuilder avec les parametres en dur sans variables pour le moment

--- src/main/java/com/wildcodeschool/original_diy/controller/MapController.java
+++ src/main/java/com/wildcodeschool/original_diy/controller/MapController.java
@@ -9,11 +9,18 @@ import reactor.core.publisher.Mono;
 @Controller
 public class MapController {
 
+    private static final String API_MAP_URL = "https://api-adresse.data.gouv.fr/search/";
+
     @GetMapping("/map")
     public String index() {
-        String url = "https://api-adresse.data.gouv.fr/search/?q=178+allee+adrienne+bolland&postcode=45770";
-        WebClient webClient = WebClient.create(url);
+       // String url = "https://api-adresse.data.gouv.fr/search/?q=178+allee+adrienne+bolland&postcode=45770";
+        WebClient webClient = WebClient.create(API_MAP_URL);
         Mono<String> call = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("q", "178", "+", "allee adrienne bolland&" )
+                        .queryParam("postcode", "45770" )
+                        .build()
+                )
                 .retrieve()
                 .bodyToMono(String.class);
         String response = call.block();
