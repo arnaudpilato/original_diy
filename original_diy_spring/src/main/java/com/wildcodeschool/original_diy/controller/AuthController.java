5e0cda97bfa7cbd9854f932b028d143c969286c1
Pil <70975556+arnaudpilato@users.noreply.github.com>
2022-02-04 17:02:48 +0100

    add jwt dependency

---
+++ original_diy_spring/src/main/java/com/wildcodeschool/original_diy/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.wildcodeschool.original_diy.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/secured")
+@CrossOrigin()
+public class AuthController {
+
+}
