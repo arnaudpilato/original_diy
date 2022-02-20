e716ad88fd59ccd9f0e1da213f8dbc0010c797d0
Pil <70975556+arnaudpilato@users.noreply.github.com>
2022-02-20 23:34:15 +0100

    add new social network in footer from spring ;

--- original_diy_spring/src/main/java/com/wildcodeschool/original_diy/controller/FooterController.java
+++ original_diy_spring/src/main/java/com/wildcodeschool/original_diy/controller/FooterController.java
@@ -1,14 +1,23 @@
 package com.wildcodeschool.original_diy.controller;
 
 import com.wildcodeschool.original_diy.entity.DiyFooter;
+import com.wildcodeschool.original_diy.entity.DiyRole;
+import com.wildcodeschool.original_diy.entity.DiyUser;
+import com.wildcodeschool.original_diy.model.ERole;
 import com.wildcodeschool.original_diy.repository.FooterRepository;
+import com.wildcodeschool.original_diy.request.FooterRequest;
+import com.wildcodeschool.original_diy.request.UserRequest;
+import com.wildcodeschool.original_diy.response.MessageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -28,6 +37,24 @@ public class FooterController {
             }
 
             return new ResponseEntity<>(socialNetworks, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<?> createsocialNetwork(@Valid @RequestBody FooterRequest footerRequest) {
+        try {
+            DiyFooter footer = new DiyFooter();
+
+            footer.setName(footerRequest.getName());
+            footer.setSocialNetworkPath(footerRequest.getSocialNetworkPath());
+            footer.setPicturePath(footerRequest.getPicturePath());
+            footer.setVisible(footerRequest.isVisible());
+
+            footerRepository.save(footer);
+
+            return new ResponseEntity<>(footer, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
