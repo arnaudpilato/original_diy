34f6f48d72b43807d0b20bb851e3881a8b17acd3
Pil <70975556+arnaudpilato@users.noreply.github.com>
2022-03-03 23:12:44 +0100

    add edit footer from angular

--- original_diy_angular/src/app/service/footer.service.ts
+++ original_diy_angular/src/app/service/footer.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { DiyFooter } from "../model/footer.model";
 import { environment } from "../../environments/environment";
+import {DiyWorkshop} from "../model/workshop.model";
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +17,19 @@ export class FooterService {
     return this.http.get<DiyFooter[]>(this.baseUrl + "all");
   }
 
+  getById(id: any): Observable<DiyWorkshop> {
+    return this.http.get(this.baseUrl + "get/" + id);
+  }
+
   delete(id: any): Observable<any> {
     return this.http.delete(this.baseUrl + "delete/" + id);
   }
 
   create(data: any): Observable<any> {
     return this.http.post(this.baseUrl + "new", data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(this.baseUrl + "edit/" + id, data);
   }
 }
