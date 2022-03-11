f82103d827f98f91f942049488453936b60d148d
Pil <70975556+arnaudpilato@users.noreply.github.com>
2022-03-11 16:22:00 +0100

    change workshop controller

--- original_diy_spring/src/main/java/com/wildcodeschool/original_diy/controller/WorkshopController.java
+++ original_diy_spring/src/main/java/com/wildcodeschool/original_diy/controller/WorkshopController.java
@@ -13,7 +13,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
 import java.util.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -82,14 +81,10 @@ public class WorkshopController {
                     workshop.getStreetNumber()).get("features").get(0).get("geometry").get("coordinates").get(1).asDouble();
             double longitude = gouvService.getAdressAsJson(workshop.getStreet(), workshop.getPostCode(),
                     workshop.getStreetNumber()).get("features").get(0).get("geometry").get("coordinates").get(0).asDouble();
+
             workshop.setLatitude(latitude);
             workshop.setLongitude(longitude);
-            // Optional<DiyUser> user = userRepository.findById(workshopRequest.getDiyUser().getId());
-            // workshop.setDiyUser(workshopRequest.getDiyUser());
             workshop.setDiyUser(workshopRequest.getDiyUser());
-
-
-            workshop.setDate(workshopRequest.getDate());
             workshopRepository.save(workshop);
             return new ResponseEntity<>(workshop, HttpStatus.CREATED);
 
