9c229c76f97a00426b990d01e129fa1e258030e3
Pil <70975556+arnaudpilato@users.noreply.github.com>
2022-05-05 22:35:24 +0200

    change comment service

--- original_diy_spring/src/main/java/com/wildcodeschool/original_diy/service/CommentService.java
+++ original_diy_spring/src/main/java/com/wildcodeschool/original_diy/service/CommentService.java
@@ -4,12 +4,21 @@ import com.wildcodeschool.original_diy.entity.DiyComment;
 import com.wildcodeschool.original_diy.entity.DiyUser;
 import com.wildcodeschool.original_diy.entity.DiyWorkshop;
 import com.wildcodeschool.original_diy.repository.CommentRepository;
+import com.wildcodeschool.original_diy.repository.UserRepository;
 import com.wildcodeschool.original_diy.repository.WorkshopRepository;
 import com.wildcodeschool.original_diy.request.CommentRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class CommentService {
@@ -18,6 +27,29 @@ public class CommentService {
 
     @Autowired
     WorkshopRepository workshopRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public void changeConfirmation(Long id) {
+        DiyComment comment = commentRepository.getById(id);
+        boolean status = comment.isConfirmed();
+
+        if (status) {
+            comment.setConfirmed(false);
+        } else {
+            comment.setConfirmed(true);
+        }
+
+        commentRepository.save(comment);
+    }
+
+    public DiyComment createComment(@Valid @RequestBody CommentRequest commentRequest, Authentication authentication) {
+        DiyUser user = userRepository.getUserByUsername(authentication.getName());
+        DiyComment comment = commentControl(commentRequest, user);
+
+        return comment;
+    }
 
     public DiyComment commentControl(CommentRequest commentRequest, DiyUser user) {
         DiyComment comment = new DiyComment();
@@ -28,7 +60,23 @@ public class CommentService {
         comment.setConfirmed(false);
         comment.setCreatedAt(new Date());
         commentRepository.save(comment);
+
         return comment;
+    }
+
+    public List<DiyComment> getCommentByWorkshop(Long id) {
+        List<DiyComment> comments = new ArrayList<>();
+        DiyWorkshop workshop = workshopRepository.getById(id);
+
+        comments.addAll(commentRepository.getCommentByWorkshop(workshop));
+
+        return comments;
+    }
+
+    public void delete(Long id) {
+        DiyComment comment = commentRepository.getById(id);
+
+        commentRepository.delete(comment);
     }
 }
 
